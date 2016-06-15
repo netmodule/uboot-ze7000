301e97335011678a5daac164d6319681fc7c0dc0
Alexandre Bard <alexandre.bard@netmodule.com>
2016-06-15 15:40:48 +0200

    Explicit usage of gnu89 standard
    This modification is needed for this old version of u-boot
    to be build with gcc5 in which default standard is now
    gnu11

--- config.mk
+++ config.mk
@@ -202,7 +202,7 @@ OBJCFLAGS += --gap-fill=0xff
 gccincdir := $(shell $(CC) -print-file-name=include)
 
 CPPFLAGS := $(DBGFLAGS) $(OPTFLAGS) $(RELFLAGS)		\
-	-D__KERNEL__
+	-D__KERNEL__ -std=gnu89
 
 # Enable garbage collection of un-used sections for SPL
 ifeq ($(CONFIG_SPL_BUILD),y)
